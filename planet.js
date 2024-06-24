cbe15b94ea1e5682453f83763f89b88451dc1b66
Sam Cross <Samuelcross2001@gmail.com>
2024-06-24 12:26:46 -0400

    Finished World Film/Charactwr

--- planet.js
+++ planet.js
@@ -7,6 +7,8 @@ let gravity;
 let orbital_Period;
 let population;
 let climate;
+let charactersUl;
+let filmsUl;
 const baseUrl = `https://swapi2.azurewebsites.net/api`;
 // Runs on page load
 
@@ -20,6 +22,8 @@ addEventListener('DOMContentLoaded', () => {
     gravity = document.querySelector('span#gravity');
     orbital_Period = document.querySelector('span#orbital_period');
     population = document.querySelector('span#population');
+    charactersUl = document.querySelector('#characters>ul');
+    filmsUl = document.querySelector('#films>ul');
     const sp = new URLSearchParams(window.location.search)
     const id = sp.get('id')
     getWorld(id)
@@ -28,7 +32,9 @@ addEventListener('DOMContentLoaded', () => {
 async function getWorld(id) {
     let world;
     try {
-        world = await fetchCharacter(id)
+        world = await fetchWorld(id);
+        world.films = await fetchFilms(world);
+        world.characters = await fetchCharacter(world);
     }
     catch (ex) {
         console.error(`Error reading world ${id} data.`, ex.message);
@@ -36,8 +42,23 @@ async function getWorld(id) {
     renderWorld(world);
 
 }
+async function fetchCharacter(world) {
+    let url = `${baseUrl}/planet/${world?.id}/characters`;
+    const characters = await fetch(url)
+        .then(res => res.json())
+    console.log("Grabbed Character");
+    console.log(characters);
+    return characters;
+}
 
-async function fetchCharacter(id) {
+async function fetchFilms(world) {
+    const url = `${baseUrl}/planet/${world?.id}/films`;
+    const films = await fetch(url)
+        .then(res => res.json())
+    return films;
+}
+
+async function fetchWorld(id) {
     let worldUrl = `${baseUrl}/planets/${id}`;
     return await fetch(worldUrl)
       .then(res => res.json())
@@ -48,11 +69,26 @@ const renderWorld = world => {
     document.title = `SWAPI - ${world?.name}`;  // Just to make the browser tab say their name
     nameH1.textContent = world?.name;
     climate.textContent = world?.climate;
-    surfaceWater.textContent = world?.surfaceWater;
+    surfaceWater.textContent = world?.surface_water;
     diameter.textContent = world?.diameter;
     rotation_Period.textContent = world?.rotation_period;
     terrain.textContent = world?.terrain;
     gravity.textContent = world?.gravity;
     orbital_Period.textContent = world?.orbital_period;
     population.textContent = world?.population;
+
+    console.log(world.films);
+    for(const film in world.films){
+        console.log(world.films[film]);
+        if(world.films[film].title){
+            const tempListItem = document.createElement("a");
+            tempListItem.setAttribute("href",`/film.html?id=${world.films[film].id}`);
+            tempListItem.innerText = `${world.films[film].title}`;
+            filmsUl.appendChild(tempListItem);
+        }
+        
+    }
+    
+    const characterLis = world?.characters?.map(character => `<li><a href="/film.html?id=${character.id}">${character.name}</li>`)
+    charactersUl.innerHTML = characterLis.join("");
 }
