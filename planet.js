b86edb1c187e139b1feb82ba120a99918c147ea5
Sam Cross <Samuelcross2001@gmail.com>
2024-06-25 10:40:05 -0400

    Changed css file to look cooler, Make fixes to how planets call json

--- planet.js
+++ planet.js
@@ -1,70 +1,50 @@
-let nameH1;
-let surfaceWater;
-let diameter;
-let rotation_Period; 
-let terrain; 
-let gravity; 
-let orbital_Period;
-let population;
-let climate;
-let charactersUl;
-let filmsUl;
+const nameH1 = document.querySelector('h1#pName');
+const surfaceWater = document.querySelector('span#surface_water');
+const diameter = document.querySelector('span#diameter');
+const rotation_Period = document.querySelector('span#rotation_period');
+const terrain = document.querySelector('span#terrain');
+const gravity = document.querySelector('span#gravity');
+const orbital_Period = document.querySelector('span#orbital_period');
+const population = document.querySelector('span#population');
+const climate = document.querySelector('span#climate');
+const charactersUl = document.querySelector('#characters>ul');
+const filmsUl = document.querySelector('#films>ul');
 const baseUrl = `https://swapi2.azurewebsites.net/api`;
+
 // Runs on page load
 
 addEventListener('DOMContentLoaded', () => {
-    nameH1 = document.querySelector('h1#pName');
-    surfaceWater = document.querySelector('span#surface_water');
-    diameter = document.querySelector('span#diameter');
-    climate = document.querySelector('span#climate');
-    rotation_Period = document.querySelector('span#rotation_period');
-    terrain = document.querySelector('span#terrain');
-    gravity = document.querySelector('span#gravity');
-    orbital_Period = document.querySelector('span#orbital_period');
-    population = document.querySelector('span#population');
-    charactersUl = document.querySelector('#characters>ul');
-    filmsUl = document.querySelector('#films>ul');
     const sp = new URLSearchParams(window.location.search)
     const id = sp.get('id')
     getWorld(id)
 });
 
 async function getWorld(id) {
-    let world;
+    let world,films,character;
+    let charactersUrl = `${baseUrl}/planet/${world?.id}/characters`;
+    let filmsUrl = `${baseUrl}/planet/${world?.id}/films`;
+    let worldUrl = `${baseUrl}/planets/${id}`;
+
+    //Getting Json Data
     try {
-        world = await fetchWorld(id);
-        world.films = await fetchFilms(world);
-        world.characters = await fetchCharacter(world);
+        world = await fetchData(worldUrl);
+        films = await fetchData(filmsUrl);
+        characters = await fetchData(charactersUrl);
     }
     catch (ex) {
         console.error(`Error reading world ${id} data.`, ex.message);
     }
-    renderWorld(world);
 
+    renderWorld(world,films,character);
 }
-async function fetchCharacter(world) {
-    let url = `${baseUrl}/planet/${world?.id}/characters`;
-    const characters = await fetch(url)
+
+async function fetchData(url){
+    const jsonData = await fetch(url)
         .then(res => res.json())
-    console.log("Grabbed Character");
-    console.log(characters);
-    return characters;
+    return jsonData;
 }
 
-async function fetchFilms(world) {
-    const url = `${baseUrl}/planet/${world?.id}/films`;
-    const films = await fetch(url)
-        .then(res => res.json())
-    return films;
-}
-
-async function fetchWorld(id) {
-    let worldUrl = `${baseUrl}/planets/${id}`;
-    return await fetch(worldUrl)
-      .then(res => res.json())
-}
-
-const renderWorld = world => {
+function renderWorld(world,films,character) {
     console.log(world);
     document.title = `SWAPI - ${world?.name}`;  // Just to make the browser tab say their name
     nameH1.textContent = world?.name;
@@ -77,18 +57,16 @@ const renderWorld = world => {
     orbital_Period.textContent = world?.orbital_period;
     population.textContent = world?.population;
 
-    console.log(world.films);
-    for(const film in world.films){
-        console.log(world.films[film]);
-        if(world.films[film].title){
+    for(const film in films){
+        if(films[film].title){
             const tempListItem = document.createElement("a");
-            tempListItem.setAttribute("href",`/film.html?id=${world.films[film].id}`);
-            tempListItem.innerText = `${world.films[film].title}`;
+            tempListItem.setAttribute("href",`/film.html?id=${films[film].id}`);
+            tempListItem.innerText = `${films[film].title}`;
             filmsUl.appendChild(tempListItem);
         }
         
     }
     
-    const characterLis = world?.characters?.map(character => `<li><a href="/film.html?id=${character.id}">${character.name}</li>`)
+    const characterLis = characters?.map(character => `<a href="/film.html?id=${character.id}">${character.name}</a>`)
     charactersUl.innerHTML = characterLis.join("");
 }
